d31167dc55d7e9f65f15c0e4dc683c76d27e57ce
Kat Maldon <katharinamaldon@gmail.com>
2019-04-16 17:21:21 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,31 +1,32 @@
-class Song 
-  
-  attr_accessor :name, :artist, :genre 
-  
-  @@count = 0 
-  
-  @@genres = [] 
-  
+class Song
+
+   attr_accessor :name, :artist, :genre
+  @@count = 0
   @@artists = []
-  
-  def initialize 
+  @@genres = []
+
+   def initialize(song_name,artist,genre)
     @name = song_name
     @artist = artist
     @genre = genre
-    @@count += 1 
+    @@count += 1
+    @@artists << artist
     @@genres << genre
-    @@artists << arist
-  end 
-  
-  def self.count 
+  end
+
+   def self.count 
     @@count
-  end 
-  
-  def self.genres 
-    @@genre.uniq
-  end 
-  
-  def self.genre_count
+  end
+
+   def self.artists
+    @@artists.uniq
+  end
+
+   def self.genres
+    @@genres.uniq
+  end
+
+   def self.genre_count
     genre_count = Hash.new(0)
     @@genres.each {|genre| genre_count[genre] += 1}
     genre_count
@@ -36,6 +37,4 @@ class Song
     @@artists.each {|artist| artist_count[artist] += 1}
     artist_count
   end
-  
 end 
-    
