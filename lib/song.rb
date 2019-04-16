9a0d654bc0473d425f994db75b4294b911f492d6
Kat Maldon <katharinamaldon@gmail.com>
2019-04-16 17:03:34 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,26 @@
+class Song 
+  
+  attr_accessor :name, :artist, :genre 
+  
+  @@count = 0 
+  
+  @@genres = [] 
+  
+  @@artists = []
+  
+  def initialize 
+    @@count += 1 
+  
+  end 
+  
+  def count 
+  end 
+  
+  def genres 
+  end 
+  
+  def genre_count
+  end 
+  
+end 
+    
